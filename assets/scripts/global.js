85d310a6c22f1b00f8285fce91fa31cbeadb3544
charles peters <charlespeters42@gmail.com>
2016-05-04 05:30:15 -0700

    importing all the modules and components

---
+++ assets/scripts/global.js
@@ -0,0 +1,8 @@
+const mercury = window.mercury || {};
+
+// TODO: rethink these pattern
+mercury.labels = require('./lib/float-label');
+mercury.modals = require('./lib/modals');
+
+
+window.mercury = mercury;
